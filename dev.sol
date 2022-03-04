5101ca5454d3140b7c945cd1dc261386341674ba
lornawanjiru <lornamuchangi@gmail.com>
2022-03-04 12:22:12 +0300

    Update dev.sol

--- dev.sol
+++ dev.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.11;
 
 contract DevProfile{
+
     address private developer;
     address[] private outsider;
-    constructor(){
-       developer = msg.sender;
-    }
-    struct dev{
+     struct dev{
         string name;
         string job_email;
         string url;
@@ -15,11 +13,32 @@ contract DevProfile{
     dev[] private devs;
     string[] comment;
     uint public amount = 1 ether;
+   
+    //events
+    event likeadded();
+    event commentsadded();
+
+
+
     //modifiers
     modifier isDev() {
         require(msg.sender == developer, "Cant add info if not a dev");
         _;
     }
+    modifier Outsider{
+       require(msg.value >= 2 ether && msg.sender != developer,"For you to like or comment you have to send the developer 2 ether or more.You cant like your own profile.");
+       payable(developer).transfer(amount);
+       _;
+    }
+
+
+    //construct
+    constructor(){
+       developer = msg.sender;
+    }
+
+    //fuctions
+
     function addinfo(string memory _name, string memory _jobemail, string memory _url) public isDev{
            dev memory devinfo = dev({name: _name, job_email: _jobemail, url: _url});
            devs.push(devinfo);
@@ -29,16 +48,16 @@ contract DevProfile{
           return (devs);
     }
     
-    function like() public payable{
-       require(msg.value >= 2 ether && msg.sender != developer,"For you to like or comment you have to send the developer 2 ether or more.You cant like your own profile.");
-       payable(developer).transfer(amount);
+    function like() public payable Outsider{
+       emit likeadded();
        outsider.push(msg.sender);
     }
 
-    function comments(string memory _comment) public payable{ 
-         payable(developer).transfer(amount);      
+    function comments(string memory _comment) public payable Outsider{ 
+         emit commentsadded();    
          comment.push(_comment);
     }
+
     function comment_info() view public returns (string[] memory){
           return comment;
     }
@@ -46,30 +65,6 @@ contract DevProfile{
           return outsider;
     }
     
-
-
-    // modifier Outsider(){
-    //     require(msg.sender != developer && msg.value > 2 ether, "For you to like or comment you have to send the developer 2 ether or more.You cant like your own profile.");
-    //     _;
-    // }
-    
-    
-    // function receiver() external payable {
-         
-    // }
-    
-    // 
-    
-    // mapping (address => string)public like;
-    // function likemes(string memory _like) public{
-    //        like[msg.sender] = _like;
-    // }
-    // mapping (address => string) public comments;
-    // function message(string memory _comments) public{
-    //       comments[msg.sender] = _comments;
-    // }
-    
-   
     
 
 }
